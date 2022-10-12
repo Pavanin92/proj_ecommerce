8c36df302aa108204473755a4bfa509915ccb32f
Gabriel Pavanin <gabrielps.map@gmail.com>
2022-10-12 16:27:53 -0300

    Changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,32 @@
+import { data } from './data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">HealthStore</a>
       </header>
-      <main>Listar produtos</main>
+      <main>
+        <h1>Featured Products</h1>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info">
+                <a href={`/product/${product.slug}`}>
+                  <p>{product.name}</p>
+                </a>
+                <p>
+                  <strong>R${product.price}</strong>
+                </p>
+                <button>Adicionar no carrinho</button>
+              </div>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
