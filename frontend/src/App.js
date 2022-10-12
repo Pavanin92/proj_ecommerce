848041a18135002b4ec3888a0ab56ab465ce569b
Gabriel Pavanin <gabrielps.map@gmail.com>
2022-10-12 13:50:53 -0300

    Aula03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">HealthStore</a>
+      </header>
+      <main>Listar produtos</main>
+    </div>
+  );
+}
+
+export default App;
